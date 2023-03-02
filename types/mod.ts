28233ce2970a6dee4c836a8d3f4f499be5404eb2
e991f665b7e62df5a54fdef19053a4e75117b89 <c@raci.sm>
2023-03-01 18:28:46 -0800

    fix(refactor): move enum to types

--- types/mod.ts
+++ types/mod.ts
@@ -4,3 +4,11 @@
 
 export type TRet = any;
 export type TObject = Record<TRet, TRet>;
+
+export enum Color {
+  TEXT_COLOR = "\x1b[38;2;160;129;226m",
+  SUCCESS = "\x1b[38;2;159;234;121m",
+  WARN = "\x1b[38;2;242;223;104m",
+  ERROR = "\x1b[38;2;242;106;104m",
+  RESET = "\x1b[0m"
+}
--- utils/Logger.ts
+++ utils/Logger.ts
@@ -2,14 +2,8 @@
  * @author e991f665b7e62df5a54fdef19053a4e75117b89 <c@raci.sm>
  */
 
-// Enums
-enum Color {
-  TEXT_COLOR = "\x1b[38;2;160;129;226m",
-  SUCCESS = "\x1b[38;2;159;234;121m",
-  WARN = "\x1b[38;2;242;223;104m",
-  ERROR = "\x1b[38;2;242;106;104m",
-  RESET = "\x1b[0m"
-}
+// Types
+import { Color } from "@types";
 
 // Logger Class
 class Logger {
